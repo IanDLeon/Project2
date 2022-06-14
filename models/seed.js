ce033558a4c9d172718d91f2ba527331a491ae05
IanDLeon <ian.reggie.deleon@gmail.com>
2022-06-14 12:05:55 -0400

    Seed to the database

--- models/seed.js
+++ models/seed.js
@@ -1,6 +1,36 @@
 // import dependencies 
 const mongoose = require('./connection');
-const Product = require('./holes');
+const Product = require('./hole');
 
 // Seed Code
 const db = mongoose.connection;
+
+// Make sure code is not run till connected
+
+db.on("open", () => {
+
+const startHoles = [
+  { username: "IanDLeon", location: "Brooklyn", severity: "Bad", fixed: false},
+  { username: "Indi", location: "Brooklyn", severity: "Bad", fixed: false},
+  { username: "Puchito", location: "Long Island", severity: "Bad", fixed: false},
+]
+// delete all products
+Product.deleteMany({})
+.then((deletedProducts)=> {
+  Product.create(startHoles)
+  .then((seededHoles)=> {
+    console.log(seededHoles)
+    db.close()
+  })
+  .catch((error) => {
+    console.log(error);
+    db.close();
+  });
+})
+  .catch((error) => {
+    console.log(error);
+    db.close();
+});
+// seed code above.
+
+})
